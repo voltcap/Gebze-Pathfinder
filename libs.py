eb73e02c56c73006147e0e02166fe8dc2517c460
Volt <121193901+voltcap@users.noreply.github.com>
2025-11-02 23:48:49 +0300

    Add initial libs.py with required imports

---
+++ libs.py
@@ -0,0 +1,12 @@
+!pip install -q osmnx networkx matplotlib
+
+import osmnx as ox
+import osmnx as ox
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.collections import LineCollection
+from matplotlib.patches import Rectangle
+from matplotlib.image import imread
+from IPython.display import Image, display
+import heapq
